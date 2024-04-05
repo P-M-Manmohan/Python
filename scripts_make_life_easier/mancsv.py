7bdb6541e3234cb4a964addd6cb7885127adb430
Manmohan P M <manu6520042@gmail.com>
2024-04-05 22:34:18 +0530

    added comments

--- scripts_make_life_easier/mancsv.py
+++ scripts_make_life_easier/mancsv.py
@@ -1,31 +1,28 @@
 import csv
 import operator as op
 
-def exctract_from_csv(file,fields):
-    list_len=len(fields)
+def exctract_from_csv(file,fields):#take fiL and list of columns required
+    list_len=len(fields)#find number of columns
     lis=[]
     for i in range(list_len):
-        lis.append([])
-    #print(list)
+        lis.append([])#create empty lists in list to append value of each coloums one list for one column
     with open (file,mode='r') as csv_file:
         csv_reader=csv.reader(csv_file)
-        header=next(csv_reader)
+        header=next(csv_reader)#take first row (header)
         field=[]
         for i in fields: 
-            field.append(header.index(i))
-        print(field)
-        group=list(zip(field, range(list_len)))
+            field.append(header.index(i))#tkae index of required fields from header
+        group=list(zip(field, range(list_len)))#group together the field nad the index of list its values will be enteed into
         for k in group:   
             print(k)
             i=k[0]
             j=k[1]
             for row in csv_reader:
-               lis[j].append(row[i])
-    print(lis)
+               lis[j].append(row[i])#trying to input values of each column in each list now only entering one column
     for i in range(len(lis[0])):
-         print(lis[0][i])
+         print(lis[0][i],"\t",print[1][i])
     positive=op.countOf(lis[1],"Positive")
-    print(positive, (len(lis[1])-positive))
+    print(positive, (len(lis[1])-positive))#find numeber of positive and negative feedback
 
 
 if __name__=="__main__":
