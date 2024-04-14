780ac65449821530ae078e4a308ef544bc1a06f3
P M Manmohan <manu6520042@gmail.com>
2024-04-14 20:39:37 +0530

    pdf merger made

--- scripts_make_life_easier/youtube_downloader.py
+++ scripts_make_life_easier/youtube_downloader.py
@@ -4,29 +4,35 @@ import moviepy.editor as mpe
 import os
 import shutil
 
-vname="clip.mp4"
-aname="audio.mp3"
-link= argv[1]
-yt=YouTube(link)
-title=yt.title
-print("Title :",yt.title)
-print("Views :",yt.views )
+def downloader(link):
+    vname="clip.mp4"
+    aname="audio.mp3"
+    yt=YouTube(link)
+    title=yt.title
+    print("Title :",yt.title)
+    print("Views :",yt.views )
 
 
-video = yt.streams.filter(subtype='mp4', res='1080p').first().download()
-os.rename(video,vname)
+    video = yt.streams.filter(subtype='mp4', res='1080p').first().download()
+    os.rename(video,vname)
 
-audio=yt.streams.filter(only_audio=True).first().download()
-os.rename(audio,aname)
-
-
-video=mpe.VideoFileClip(vname)
-audio=mpe.AudioFileClip(aname)
-final=video.set_audio(audio)
+    audio=yt.streams.filter(only_audio=True).first().download()
+    os.rename(audio,aname)
 
 
-final.write_videofile(f"{title}.mp4",codec="libx264")
-os.remove(vname)
-os.remove(aname)
+    video=mpe.VideoFileClip(vname)
+    audio=mpe.AudioFileClip(aname)
+    final=video.set_audio(audio)
 
-shutil.move(f"{title}.mp4", "/home/manmohan/Documents/videos")
+
+    final.write_videofile(f"{title}.mp4",codec="libx264")
+    os.remove(vname)
+    os.remove(aname)
+
+    shutil.move(f"{title}.mp4", "/home/manmohan/Documents/videos")
+
+
+if __name__=="__main__":
+    link= argv[1]
+    downloader(link)
+
