96bc1ff161f265bb8706c228f44b2922d87f16f8
P M Manmohan <manu6520042@gmail.com>
2024-05-15 21:44:11 +0530

    stared script for friend

---
+++ scripts_make_life_easier/game_recorder.py
@@ -0,0 +1,3 @@
+import subprocess
+
+subprocess.call('/home/salami/.local/share/flatpak/app/com.obsproject.Studio/x86_64/stable/71d974e21fd96594d6ce66314962435a46674e1c441abcc9a6d64cbe5a5f7eda/files/share/obs')
